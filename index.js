5ff1b5087f69bbd705e56fc08aa4bb9b26bb61c4
Elena <inculet98@mail.ru>
2023-09-27 17:13:01 +0300

    adaugare modificari

---
+++ index.js
@@ -0,0 +1,50 @@
+// const ziuaSaptaminii = 'o zi ordinara';
+
+// switch (ziuaSaptaminii) {
+//   case 'Luni' === ziuaSaptaminii:
+//     console.log('Astazi este luni!')
+//   case 'Marti' === ziuaSaptaminii:
+//     console.log('Astazi este marti!')
+//     break;
+//   case 'Miercuri' === ziuaSaptaminii:
+//     console.log('Astazi este miercuri!')
+//     break;
+//   case 'Joi' === ziuaSaptaminii:
+//     console.log('Astazi este Joi!')
+//     break;
+//   case 'Vineri' === ziuaSaptaminii:
+//     console.log('Astazi este Vineri!')
+//     break;
+//   case 'Sambata' === ziuaSaptaminii:
+//     console.log('Astazi este Sambata!')
+//     break;
+//   case 'Duminica' === ziuaSaptaminii:
+//     console.log('Astazi este Duminica!')
+//     break;
+//   default:
+//     console.log('Astazi este luni!')
+// }
+
+
+// const variabila = new String('str')
+// if (variabila >= 10) {
+//   console.log(`${variabila >= 10}`)
+// } else if (variabila <= 10) {
+//   console.log(`${variabila <= 10}`)
+// }
+
+
+// const variabila = 'astazi este miercuri'
+// console.log(variabila.toUpperCase())
+
+
+// const variabila = 'Astazi este Miercuri'
+// console.log(variabila.toLowerCase())
+
+
+// const arr = ['Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri']
+// console.log(arr.slice(2))
+
+
+const arr = ['Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri']
+console.log(arr.splice(2))
